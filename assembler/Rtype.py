3f57894f06e66522cd0c63e6202984b214255233
Reza Nematollahi <nematollahi1381@gmail.com>
2022-07-06 02:51:37 +0430

    add sll and srl in datapath

--- assembler/Rtype.py
+++ assembler/Rtype.py
@@ -5,6 +5,6 @@ def r_type_instruction(funct, rs, rt, rd, shamt):
     with open('registers.json', 'r') as json_file:
             registers = json.load(json_file)
     if rs in registers and rt in registers and rd in registers:
-        shamt = bin(shamt)[2:]
+        shamt = bin(shamt)[2:][-5:]
         return 6*'0' + registers[rs] + registers[rt] + registers[rd] + (5-len(shamt))*'0' + shamt + funct
 
