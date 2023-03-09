9e001dc40ed645b2ed406a10f8193f33bcca8763
Reza Nematollahi <76822523+Reza-81@users.noreply.github.com>
2023-03-09 11:59:48 +0330

    Update mux.py

--- mips_cpu/mux.py
+++ mips_cpu/mux.py
@@ -21,6 +21,3 @@ def mux_16x1(a : int, b : int, c : int, d : int, e : int, f : int, g : int, h : 
     mux_2 = mux_8x1(i, j, k, l, m, n, o, p, selector[1:])
     mux_3 = mux_2x1(mux_1, mux_2, int(selector[0]))
     return mux_3
-
-# print(mux_2x1(1, 0, 0))
-# print(mux_16x1(1,0,1,1,1,1,1,1,1,1,1,1,0,1,1,0,'0001'))
